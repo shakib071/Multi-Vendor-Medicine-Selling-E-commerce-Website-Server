89f1cc00d80c48ac42031340aaa250fbb31db9ee
shakib071 <shakibhasan071@gmail.com>
2025-08-12 08:02:03 +0600

    added post method for users

--- index.js
+++ index.js
@@ -32,11 +32,43 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const usersCollection = client.db('medicineSellDB').collection('users');
 
     app.get('/',(req,res)=> {
     res.send("Hello world from server");
     });
 
+    
+
+
+    //save user
+
+    app.post('/users',async(req,res)=> {
+        const {username,email,role,photoURL,uid} = req.body;
+
+        try{
+            const existingUser = await usersCollection.findOne({email});
+
+            if(existingUser){
+                return res.status(200).send({message: 'User Already exists'});
+            }
+            const newUser = {
+                uid,
+                email,
+                username,
+                role,
+                photoURL,
+                createdAt: new Date(),
+                
+            };
+
+            const result = await usersCollection.insertOne(newUser);
+            res.send(result);
+        }
+        catch(error) {
+            res.status(500).send({message: 'server error'});
+        }
+    });
 
 
 
